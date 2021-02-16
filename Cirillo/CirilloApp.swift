4ef0bf7934ef0a19a8ac2632789ecbcecda875da
viere1234 <69096696+viere1234@users.noreply.github.com>
2021-02-16 14:51:47 +0800

    Initial Commit

---
+++ Cirillo/CirilloApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirilloApp.swift
+//  Cirillo
+//
+//  Created by 張智堯 on 2021/2/16.
+//
+
+import SwiftUI
+
+@main
+struct CirilloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
